412e8a160a8938800c7ccbca24abf683c5c91832
Roman Masyagin <rmasyagin@webdevs.us>
2019-08-19 20:02:15 +0300

    implementation PDF rendering

--- pdf.js
+++ pdf.js
@@ -12,7 +12,7 @@ async function main() {
       '--no-sandbox', 
       '--start-maximized', 
       //'--user-data-dir=$HOME/.config/google-chrome/',
-      '--remote-debugging-port=9223'
+      '--remote-debugging-port=9224'
     ],
     //executablePath:"/opt/google/chrome/google-chrome",
     //userDataDir: "/home/rmasyagin/.config/google-chrome/"
@@ -25,21 +25,33 @@ async function main() {
     if (!url) {
       return res.send(`
         <p>Please provide URL as GET parameter, for example: <a href="/?url=https://example.com">?url=https://example.com</a></p>
-        <p></p>Bookmarklet: <a href="javascript:open('http://localhost:7777?url='+location.href)">Screenshot</a></p>
+        <p></p>Bookmarklet: <a href="javascript:open('http://localhost:8888?url='+location.href)">Pdf</a></p>
       `);
     }
 
     const page = await browser.newPage();
     await page.goto(url);
+    await page.emulateMedia('screen');
 
     page.setViewport({width:1920, height:1080});
-    const imageBuffer = await page.screenshot({fullPage: true});
+    
+    const pdfBuffer = await page.pdf({
+      printBackground: true,
+      // width:1920,
+      // height:1080,
+      //format: 'A4',
+      // format: 'Letter', 
+      // PreferCSSPageSize: true 
+    });
 
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
+    let hostName = url.replace(/(https?:\/\/(?:www\.)?([^\/]*)\/.*$)/,'$2');
+
+    res.set('Content-Type', 'application/pdf');
+    res.set('Content-Disposition', 'inline; filename="'+hostName+'.pdf"');
+    res.send(pdfBuffer);
   });
 
-  const server = app.listen(process.env.PORT || 7777, err => {
+  const server = app.listen(process.env.PORT || 8888, err => {
     if (err) {
       return console.error(err);
     }
